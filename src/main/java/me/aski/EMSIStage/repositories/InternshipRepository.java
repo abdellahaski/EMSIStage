8630ae5d30838fe4d7b9e02e637b277179ff11fb
Abdellah ASKI <abdellahaski@gmail.com>
2018-07-17 19:05:29 +0100

    a

--- src/main/java/me/aski/EMSIStage/repositories/InternshipRepository.java
+++ src/main/java/me/aski/EMSIStage/repositories/InternshipRepository.java
@@ -20,7 +20,7 @@ public interface InternshipRepository extends JpaRepository<Internship, Integer>
     @Query("select COUNT(i) from Internship i where i.student= :student")
     int countInternshipByStudent(@Param("student") Student student);
 
-    //@Query("select i from Internship i where i.student= :student order by i.dateFrom desc")
+    //@Query("select i from Internship i where i.student= :student order by i.dateFrom desc");
     @Query(value="SELECT * FROM internship where student = :id ORDER BY date_from desc LIMIT 1", nativeQuery = true)
     Internship getLastInternshipByStudent(@Param("id") int id);
 
